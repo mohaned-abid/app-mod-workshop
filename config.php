32b2c8901d00c9698921f45995696ee784e730cc
mohaned abid <mohanedabid47@gmail.com>
2025-02-20 15:07:22 +0000

    update config

--- config.php
+++ config.php
@@ -1,15 +1,17 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+// **HIGHLY INSECURE - DO NOT DO THIS IN PRODUCTION**
+$db_host = 'test-project-451514:europe-west8:appmod-phpapp'; // Replace with your instance connection name
+$db_name = 'image_catalog'; //Replace with your database name
+$db_user = 'appmod-phpapp-user'; //Replace with your database username
+$db_pass = 'admin'; //Replace with your database password
 
 try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    //Use the PDO MySQL socket connection. This is generally preferred for better performance and security in Google Cloud.
+    $pdo = new PDO("mysql:unix_socket=/cloudsql/{$db_host};dbname={$db_name}", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
-    die("Errore di connessione: " . $e->getMessage());
+    die("Error connecting to Cloud SQL: " . $e->getMessage());
 }
 
 session_start();
